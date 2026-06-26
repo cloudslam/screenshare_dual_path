e8937fd5821f0a18aef2792137b837db87df4036
cloudslam <55304876+cloudslam@users.noreply.github.com>
2026-06-26 17:26:46 +0800

    Add button press success feedback

---
+++ app/src/main/java/com/example/screenshare/dualpath/GlChildActivityTwo.java
@@ -0,0 +1,8 @@
+package com.example.screenshare.dualpath;
+
+public class GlChildActivityTwo extends BaseGlChildActivity {
+    @Override
+    protected int getAnimationMode() {
+        return 2;
+    }
+}
---
+++ app/src/main/java/com/example/screenshare/dualpath/ProjectionControl.java
@@ -0,0 +1,11 @@
+package com.example.screenshare.dualpath;
+
+public final class ProjectionControl {
+    public static final String ACTION_STOP_PROJECTION = "com.example.screenshare.dualpath.action.STOP_PROJECTION";
+    public static final String EXTRA_DISPLAY_ID = "com.example.screenshare.dualpath.extra.DISPLAY_ID";
+    public static final String EXTRA_ACTIVITY_CLASS = "com.example.screenshare.dualpath.extra.ACTIVITY_CLASS";
+    public static final int DISPLAY_ID_NONE = Integer.MIN_VALUE;
+
+    private ProjectionControl() {
+    }
+}
